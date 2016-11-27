8b4083f10a2a2db308db926a8da78691b494366b
iYaipH <irimamekun@gmail.com>
2016-11-27 16:07:10 +0900

    Setup入力項目にQuestionIDの追加

--- Setup.rb
+++ Setup.rb
@@ -2,10 +2,12 @@ require './judge'
 
 class Setup
   def main
-    lang = select_lang
-    user_name = input_user_name
-    js = JudgeSystem.new(user_name, lang); js.main
+    lang        = select_lang
+    user_name   = input_user_name
+    question_id = input_question_id
+    js = JudgeSystem.new(user_name, lang, question_id); js.main
   end
+
   def select_lang
     puts "使用した言語を入力してください"
     lang = {
@@ -15,9 +17,15 @@ class Setup
     }[gets.chomp]
     return lang
   end
+
   def input_user_name
     puts "あなたのGitHubUserNameを入力してください"
     return gets.chomp
+  end
+
+  def input_question_id
+    puts "ジャッジするQuestionIdを入力してください(20161125)"
+    return gets.to_i
   end
 end
 

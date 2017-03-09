f355ae66663520c65d11cdaa6d84960259de28f2
JpnLavender <irimamekun@gmail.com>
2017-03-09 23:58:12 +0900

    GitHubLoginできるように

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,repo"
-  puts "*" * 30
-  p ENV['GITHUB_KEY']
-  p ENV['GITHUB_SECRET']
-  puts "-" * 30
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #  get 'auth/:provider/callback', to: 'sessions#callback'
-  get '/logout', to: 'sessions#destroy'
+  get 'auth/:provider/callback', to: 'sessions#callback'
+  get '/logout', to: 'sessions#destroy', as: :logout
 
   root 'home#top'
 end

ac30089268803708c46155a2b18dc3c2ed4d93dc
JpnLavender <irimamekun@gmail.com>
2017-04-24 17:56:08 +0900

    Update Test ShellScript

--- API/CurlTest.sh
+++ API/CurlTest.sh
@@ -3,7 +3,7 @@ curl \
     -X POST \
     http://localhost:1323/answer-data \
     -H 'Content-Type: application/json' \
-    -d '{"question_id": "1", "answer_data": "puts gets.chomp", "lang": "ruby"}'
+    -d '{"question_id": "1", "answer_data": "puts gets.chomp.to_i", "lang": "rb"}'
 
 # curl \
 #     -X POST \

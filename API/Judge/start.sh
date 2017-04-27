bc492621846fec10c28aaa2f62daff82459595a1
JpnLavender <irimamekun@gmail.com>
2017-04-27 19:25:56 +0900

    Create Docker start shellscript file

---
+++ API/Judge/start.sh
@@ -0,0 +1,10 @@
+################################
+#Test
+imageName="ruby:2.3.4-alpine" # imageName=$1
+run_cmd=("${1} ${2}")
+id=$3
+################################
+
+docker run --name test \
+    -i -v $(pwd)/Judge/WorkSpace:/WorkSpace \
+    -t $imageName ./run.sh $run_cmd $id

1a27c2f35e40ae965a4bff4b201f9d8b7d085d9b
JpnLavender <irimamekun@gmail.com>
2017-05-02 01:31:16 +0900

    ADD DockerRunTest Select function

--- API/tests/DockerRunTest.sh
+++ API/tests/DockerRunTest.sh
@@ -16,6 +16,28 @@ ruby() {
         echo "Error to Process there Ruby"
     fi
 }
-############################
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-$1;
+############################
+# Select Tester 
+############################
+CMDNAME=`basename $0`
+
+while getopts as: OPT
+do
+  case $OPT in
+    "a" ) FLG_A="TRUE" ;;
+    "s" ) FLG_S="TRUE" ; VALUE_S="$OPTARG" ;;
+      * ) echo "Usage: $CMDNAME [-a] [-s VALUE]" 1>&2
+          exit 1 ;;
+  esac
+done
+
+if [ "$FLG_A" = "TRUE" ]; then
+    ruby
+fi
+
+if [ "$FLG_S" = "TRUE" ]; then
+  $VALUE_S
+fi
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%

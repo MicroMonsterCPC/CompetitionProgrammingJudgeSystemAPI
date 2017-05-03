f07e5b784a48a476acb15a9fcd0cc0fef2729331
JpnLavender <irimamekun@gmail.com>
2017-05-03 17:50:53 +0900

    SuperCommit

--- API/Judge/start.sh
+++
@@ -1,9 +0,0 @@
-################################
-imageName="ruby:2.3.4-alpine" # imageName=$1
-run_cmd=("${1} ${2}")
-id=$3
-################################
-
-docker run --name test \
-    -i -v $(pwd)/Judge/WorkSpace:/WorkSpace \
-    -t $imageName /WorkSpace/judge_run.sh $run_cmd $id

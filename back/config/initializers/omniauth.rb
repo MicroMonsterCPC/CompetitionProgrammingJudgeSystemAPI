dfc33eec8093195503c2421635f93d90b0fbd57c
JpnLavender <irimamekun@gmail.com>
2017-05-19 15:47:43 +0900

    GitHubのTokenが無い場合に例外を出すように

--- back/config/initializers/omniauth.rb
+++ back/config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,repo"
+  provider :github, ENV.fetch('GITHUB_KEY'), ENV.fetch('GITHUB_SECRET'), scope: "user,repo"
+  
 end

435c0b24bf68f53aa6f720dacaf94e6dacf06cde
JpnLavender <irimamekun@gmail.com>
2017-05-24 23:54:58 +0900

    TemplateEnginの導入

--- api/Main.go
+++ api/Main.go
@@ -6,14 +6,30 @@ import (
 	"./apps/JudgementController"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"html/template"
+	"io"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
 	e := echo.New()
 
 	//=======Middleware=======
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	//=======TemplateEngine=======
+	e.Renderer = t
 
 	//=======Routers=======
 	e.GET("/", HomeController.Root)
